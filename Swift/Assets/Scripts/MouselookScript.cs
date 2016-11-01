d2d2a23dc71a841c8cd6755111f664058718f8bb
Rob Macias <robmcs7117@gmail.com>
2016-11-01 15:09:47 -0700

    FPS Begin
    worked on cameras and movement. switching to desktop to furter work on
    this

---
+++ Swift/Assets/Scripts/MouselookScript.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camer-Control/MouselookScript")]
+public class MouselookScript : MonoBehaviour {
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15f;
+    public float sensitivityY = 15f;
+
+    public float minimumX = -360f;
+    public float maximumX = 360f;
+    public float minimumY = -60f;
+    public float maximumY = 60f;
+    float rotationX = 0f;
+    float rotationY = 0f;
+    Quaternion originalRotation;
+    float parentZ;
+    
+       
+    // Use this for initialization
+    void Start () {
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+            originalRotation = transform.localRotation;
+            
+        }
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+
+            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation = originalRotation * xQuaternion;
+        }
+        else if(axes == RotationAxes.MouseY)
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+            Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+            transform.localRotation = originalRotation * yQuaternion;
+        }
+	}
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360f)
+            angle += 360f;
+        if(angle > 360f)
+        {
+            angle -= 360f;
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+}
